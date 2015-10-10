55aa19b678614e498dfed34bc987cd25a77529d0
Oleksii Fedorov <waterlink000@gmail.com>
2015-10-10 21:26:01 +0200

    [systems/movement_by_camera] Disallow moving camera over blocks_movement entities

--- src/systems/movement_by_camera.cr
+++ src/systems/movement_by_camera.cr
@@ -3,19 +3,39 @@ module Ton
     def update
       return unless camera?
       return unless party?
+      return if same_position?
 
-      party.position.bind do |position|
-        position.x = camera.position!.x
-        position.y = camera.position!.y
+      return restore_camera unless nothing_under_camera?
+
+      party.position!.x = camera.position!.x
+      party.position!.y = camera.position!.y
+    end
+
+    def restore_camera
+      camera.position!.x = party.position!.x
+      camera.position!.y = party.position!.y
+    end
+
+    def nothing_under_camera?
+      world.each.blocks_movement do |entity|
+        next unless entity.position?
+        next unless Position.same_position?(camera.position!, entity.position!)
+        return false
       end
+
+      true
+    end
+
+    def same_position?
+      Position.same_position?(party.position!, camera.position!)
     end
 
     def camera?
-      world.each.camera.any?
+      world.each.camera.any? && camera.position?
     end
 
     def party?
-      world.each.character.any?
+      world.each.character.any? && party.position?
     end
 
     def camera
